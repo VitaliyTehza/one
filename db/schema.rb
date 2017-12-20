cc1a1c46b8d9427f4e44c8b36eaf338f55fc60fc
VitaliyTehza <w.tehza@gmail.com>
2017-12-20 15:45:49 +0200

    REGISTR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171215084104) do
+ActiveRecord::Schema.define(version: 20171220124629) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20171215084104) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
